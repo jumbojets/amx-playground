98a205d19abd1cf24c9123f8f5d3a52537422b41
James Taylor <54148103+jumbojets@users.noreply.github.com>
2023-12-27 13:27:45 -0500

    small (play) matmul works

--- play.c
+++ play.c
@@ -7,7 +7,7 @@
 #include "util.h"
 
 // mini matrix multiplication
-#define N 4
+#define N 8
 
 int16_t A[N*N]; // assume that it is transposed
 int16_t B[N*N];
@@ -18,18 +18,16 @@ int main() {
   rand_array(B,N*N);
 
   print_mat(A,N,N);
+  printf("\n");
   print_mat(B,N,N);
+  printf("\n");
 
   AMX_SET();
 
   for (uint64_t i = 0; i < N; i++) {
-    AMX_LDX(PMASK & (uint64_t)(B + 2*N*i) | (i << 56));
-    AMX_LDY(PMASK & (uint64_t)(A + 2*N*i) | (i << 56));
-  }
-
-  for (uint64_t i = 0; i < N; i++) {
-    for (uint64_t j = 0; j < N; j++)
-      AMX_MAC16((j*64) | ((i*64) << 9));
+    AMX_LDX(PMASK & (uint64_t)(B + N*i) | (i << 56));
+    AMX_LDY(PMASK & (uint64_t)(A + N*i) | (i << 56));
+    AMX_MAC16((i*64) | ((i*2*64) << 9));
   }
 
   for (uint64_t i = 0; i < N; i++)
--- util.h
+++ util.h
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#include "amx.h"
+
 #define PMASK 0xffffffffffffff
 
 void rand_array(int16_t *arr, int size) {
@@ -12,7 +14,22 @@ void rand_array(int16_t *arr, int size) {
 void print_mat(int16_t *arr, int rows, int cols) {
   for (int i = 0; i < rows; i++) {
     for (int j = 0; j < cols; j++)
-      printf("%d, ", arr[i*rows+j]);
+      printf("%d, ", arr[i*cols+j]);
     printf("\n");
   }
+}
+
+void read_x(int16_t ret[32]) {
+  for (uint64_t i = 0; i < 8; i++)
+    AMX_STX(PMASK & (uint64_t)(ret+i*32) | (i << 56));
+}
+
+void read_y(int16_t ret[32]) {
+  for (uint64_t i = 0; i < 8; i++)
+    AMX_STY(PMASK & (uint64_t)(ret+i*32) | (i << 56));
+}
+
+void read_z(int16_t ret[2048]) {
+  for (uint64_t i = 0; i < 64; i++)
+    AMX_STZ(PMASK & (uint64_t)(ret+i*32) | (i << 56));
 }
